eaf580a8081f44ceaa3eb22718e1b6a8b1ecc096
doyeonHa <w2113@e-mirim.hs.kr>
2023-03-15 10:19:06 +0900

    함수 화살표 함수로 바꾸기

--- 문법.js
+++ 문법.js
@@ -31,8 +31,7 @@ sub = (n1, n2) => n1 - n2;
 result = sub(3, 2);
 console.log(result);    //1
 
-square = (num) => {
-    return num*num;
-}
+square = (num) => num*num;
 
 result = square(3);
+console.log(result);
