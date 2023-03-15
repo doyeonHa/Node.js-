8ca080202ab0d3b78100d3902ccab56bba3864b6
doyeonHa <w2113@e-mirim.hs.kr>
2023-03-15 10:02:43 +0900

    함수 body가 return 하나일 때

--- 문법.js
+++ 문법.js
@@ -16,13 +16,17 @@ function add(n1, n2) {
 result = add(1, 2);
 console.log(result)
 
-plus = add;
+plus = add;  // plus도 add의 역할을 할 수 있음
 result = plus(3, 4);
-console.log(result);
+console.log(result);  // 7
 
-sub = function(n1, n2) {
-    return n1-n2;
-}
+// sub = function(n1, n2) {
+//     return n1-n2;
+// }
 
+// result = sub(3, 2);
+// console.log(result)
+
+sub = (n1, n2) => n1 - n2;
 result = sub(3, 2);
-console.log(result)
+console.log(result);    //1
