90bbeb276c11001945a9f15a899ee1b4bb44deba
doyeonHa <w2113@e-mirim.hs.kr>
2023-04-19 10:03:16 +0900

    파일복사, 삭제, 이름추출

--- fs.js
+++ fs.js
@@ -10,6 +10,12 @@ console.log(files);
 result = __dirname + '\\' + files[1]
 console.log(result);
 
+console.log(path.basename(result)); // path.basename() 파일명만 추출
+
+const copiedFile = path.join(__dirname, '복사한파일.js')
+fs.copyFileSync(result, copiedFile); // 파일 복사
+fs.unlinkSync(copiedFile); // 파일 삭제
+
 /// 해당 파일에 대한 정보를 알 수 있음
 console.log(fs.statSync(result).isFile());      // true
 console.log(fs.statSync(result).isDirectory()); // false
