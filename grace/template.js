86217cc2dbd5a8d98469e740ab777d4c500e3a56
doyeonHa <w2113@e-mirim.hs.kr>
2023-05-17 10:05:53 +0900

    renderFile을 프로미스 방식으로

--- grace/template.js
+++ grace/template.js
@@ -16,11 +16,8 @@ const server = http.createServer(function(req, res) {
   if (req.url === '/') {
     // renderFile(ejs 대상경로, ejs에 넘겨줄 데이터, 이후에 실행할 callback)
     ejs.renderFile(path.join(__dirname, 'template', 'index.ejs'),
-    {name: index.name, here: index.here},
-    // 데이터를 넘겨받은 ejs코드(data)를 클라이언트에게 보낸다.
-    function(err, data) {
-      res.end(data);
-    });
+    {name: index.name, here: index.here})
+    .then((data) => res.end(data)); // 데이터를 넘겨받은 ejs코드(data)를 클라이언트에게 보낸다.
   } else if(req.url === '/food') {
     fs.createReadStream(path.join(__dirname, 'html', 'food.html')).pipe(res);
   } else {
