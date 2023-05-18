cda96b14232c10ca06382177db02ef3d1d558d35
doyeonHa <w2113@e-mirim.hs.kr>
2023-05-18 15:00:33 +0900

    객체의 속성과 값이 같은 경우 한 번만 언급해주면 된다

--- grace/template.js
+++ grace/template.js
@@ -9,11 +9,10 @@ const index = {
   here: '미림'
 };
 
-const food = {
-  food: '음식',
-  drink: '요거트 스무디',
-  like: '좋아'
-}
+// food.ejs에 넘겨줄 data
+const food = "음식";
+const drink = "요거트 스무디";
+const like = "좋아";
 
 const server = http.createServer(function(req, res) {
   console.log(req.url);
@@ -26,7 +25,7 @@ const server = http.createServer(function(req, res) {
     .then((data) => res.end(data)); // 데이터를 넘겨받은 ejs코드(data)를 클라이언트에게 보낸다.
   } else if(req.url === '/food') {
     ejs.renderFile(path.join(__dirname, 'template', 'food.ejs'),
-    {food: food.food, drink: food.drink, like: food.like})
+    {food, drink, like})
     .then((data) => res.end(data));
   } else {
     fs.createReadStream(path.join(__dirname, 'html', '404.html')).pipe(res);
