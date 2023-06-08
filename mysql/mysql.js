9d535d480d015e5c83d9499cf9a72733b10e8259
doyeonHa <w2113@e-mirim.hs.kr>
2023-06-08 14:01:04 +0900

    INSERT

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -2,27 +2,44 @@ const mysql = require('mysql2/promise');
 const path = require('path');
 
 require('dotenv').config({
-  path: path.join(__dirname, '.env')
+    path: path.join(__dirname, '.env')
 });
 
 // mysql 시스템 정보(절대로 공개하면 안됨)
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DB,
-  port: 3310,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DB,
+    port: 3310,
 });
 
+const insertData = {
+    name: 'bengi',
+    lane: 'jg',
+    team: 't1',
+    kills: 5,
+    deaths: 3,
+    assists: 8
+};
+
+pool.query("INSERT INTO player SET ?", insertData)
+    .then(() => {
+        console.log('INSERT 성공')
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
 pool.query("SELECT * FROM player")
-  .then(([results]) => {
-    for(const r of results) {
-      console.log(r);
-    }
-  })
-  .catch(err => {
-    console.error(err);
-  });
+    .then(([results]) => {
+        for (const r of results) {
+            console.log(r);
+        }
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
 // pool.query("SELECT * FROM player", (err, results) => {
 //     for (const r of results)
