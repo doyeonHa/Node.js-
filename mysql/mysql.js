583a56555357affdb3976601224f48df5e417a28
doyeonHa <w2113@e-mirim.hs.kr>
2023-06-08 13:52:28 +0900

    promise 방식으로 전환

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -1,16 +1,27 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
+const path = require('path');
+
 require('dotenv').config({
-    path: path.join(__dirname, '.env')
+  path: path.join(__dirname, '.env')
 });
 
+// mysql 시스템 정보(절대로 공개하면 안됨)
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DB,
-    port: 3310
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DB,
+  port: 3310,
 });
 
-pool.query("SELECT * FROM player", (err, results) => {
-    console.log(results);
-});
+pool.query("SELECT * FROM player")
+  .then(([results]) => {
+    for(const r of results) {
+      console.log(r);
+    }
+  })
+
+// pool.query("SELECT * FROM player", (err, results) => {
+//     for (const r of results)
+//         console.log(results);
+// });
